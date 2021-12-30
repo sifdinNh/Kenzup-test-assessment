a207303418d8e23a6f178fd629eb3f7f6b9dff23
sifdinNh <sifdinnh@gmail.com>
2021-12-30 13:00:47 +0100

    setting up payements models User,Transactions

---
+++ Payment_api/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from .models import User
+
+
+# Register your models here.
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="mot de passe", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="confirmation de mot de passe", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username','first_name','last_name',)
+
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("les mots de passe no correspondent pas")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField
+
+    class Meta:
+        model = User
+        fields = ('email','last_name','first_name', 'password', 'is_active', 'is_superuser', 'is_staff')
+
+    def clean_password(self):
+        return self.initial["password"]
--- Payment_api/models.py
+++ Payment_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+import uuid
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 # Create your models here.
+
+
+
+class User(AbstractUser,PermissionsMixin):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    email=models.EmailField(unique=True)
+    first_name=models.CharField(max_length=30,null=True,blank=True)
+    last_name=models.CharField(max_length=30,null=True,blank=True)
+    balance = models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
+    date_joined=models.DateField(auto_now_add=True)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+
+    def get_balance(self):
+        return self.balance
+        
+    def __str__(self):
+        return str(self.username)
+
+class Transaction(models.Model):
+    sender = models.ForeignKey(User,on_delete=models.PROTECT,related_name='sender')
+    receiver = models.ForeignKey(User,on_delete=models.PROTECT,related_name='receiver')
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
+
+
+    
